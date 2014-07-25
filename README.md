d4657c3946bd3317ee8e6b33e8a76ddd241869e3
Julio Castillo <juliocc@gmail.com>
2014-07-25 14:15:52 +0200

    WTFPL

--- README.md
+++ README.md
@@ -12,5 +12,4 @@ FIXME
 
 Copyright © 2014 Julio Castillo
 
-Distributed under the Eclipse Public License either version 1.0 or (at
-your option) any later version.
+Distributed under the WTFPL
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject rosalind "0.1.0-SNAPSHOT"
   :description "Solutions to the rosalind problems"
   :url "http://github.com/juliocc/rosalind"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Do What the Fuck You Want to Public License"
+            :url "http://www.wtfpl.net/about/"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
